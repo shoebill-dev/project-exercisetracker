5239884c7f8d27f94c8bac681251a6aac57e3399
Johan <dev@shoebill.nl>
2022-09-29 20:45:26 +0200

    add logs route

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
-require('../db/mongoose')
+require("../db/mongoose");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-  },
+  }
 });
 
-const User = mongoose.model("user", userSchema);
+userSchema.virtual('exercises', {
+  ref: 'Exercise',
+  localField: '_id',
+  foreignField: 'owner'
+})
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const User = require("../models/user");
 const Exercise = require("../models/exercise");
+const User = require("../models/user");
 
 const router = new express.Router();
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
@@ -36,6 +36,42 @@ router.post("/api/users/:_id/exercises", async (req, res) => {
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json(error);
+  }
+});
+
+router.get("/api/users/:_id/logs", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const { from, to, limit } = req.query;
+    const fromDate = from ? new Date(from) : new Date(-8640000000000000);
+    const toDate = to ? new Date(to) : new Date(8640000000000000);
+    const user = await User.findById(_id)
+      .populate({
+        path: "exercises",
+        limit,
+        match: { date: { $gte: fromDate, $lte: toDate } },
+      })
+      .exec();
+    const count = user.exercises.length;
+
+    responseExercises = user.exercises.map((exercise) => {
+      return {
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString(),
+      };
+    });
+
+    const response = {
+      username: user.username,
+      count,
+      _id: user._id,
+      log: responseExercises,
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json(error)
   }
 });
 
